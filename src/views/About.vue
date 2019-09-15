96e7115514b6902165ca56a54f45e3df93b150fb
史鑫磊 <shi_xinlei@qq.com>
2019-09-15 23:24:37 +0800

    0.0.01

---
+++ src/views/About.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="about">
+    <router-link to="/about">about</router-link>
+    <router-view></router-view>
+    <video ></video>
+    <button @click="BtnClickZ">拍照</button>
+    <canvas></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"about",
+  data(){
+    return {
+
+    }
+  },
+  mounted(){
+    let promise = this.$route.params.msg
+    // let video = this.$el.lastElementChild;
+    let video = this.$el.children[1]
+    promise.then(function (MediaStream) {
+            video.srcObject = MediaStream;
+            video.play();
+    })
+  },
+  methods:{
+    BtnClickZ(){
+      let canvas = this.$el.children[3];
+      let video = this.$el.children[1]
+      canvas.height = 800
+      canvas.width = 600
+      let ctx = canvas.getContext('2d');
+      ctx.drawImage(video,0,0, 700, 800);
+
+    }
+  }
+}
+</script>
+<style  scoped>
+  
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+      <button @click="BtnCLick" >点击</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  data(){
+    return {
+      promise:""
+    }
+  },
+  methods:{
+    BtnCLick(){
+       let promise = navigator.mediaDevices.getUserMedia({
+            video: {width: 500, height: 500},
+            audio: true
+        });
+        this.promise = promise;
+        this.$router.push({
+          name:"about",
+          path:"/about",
+          params:{
+            msg:this.promise
+          }
+        })
+        
+    }
+  }
+}
+</script>
